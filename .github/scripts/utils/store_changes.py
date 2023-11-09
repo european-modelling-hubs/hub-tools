a0855db95f093267e170906f7756560dcbf3c444
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-11-09 16:48:39 +0100

    Update store_changes.py

--- .github/scripts/utils/store_changes.py
+++ .github/scripts/utils/store_changes.py
@@ -24,6 +24,7 @@ def storeForecasts (forecasts):
             model_entry["changes"].append(forecast)
 
     if out_data['models']:
+        print(f"Current path: {os.getcwd()}")
         db_path = os.path.join("..", "..", "..", "..", ".github", "data-storage", "changes_db.json")
         # db_path = db_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "changes_db.json")
         updateForecastsJson(db_path, out_data)
