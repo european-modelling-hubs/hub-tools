3cb5900f8a0fca3caf017a8ecde7d28b9b27e97d
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-11-09 20:42:28 +0100

    Update store_changes.py

--- code/store_changes.py
+++ code/store_changes.py
@@ -160,5 +160,6 @@ def store(to_store):
 
 if __name__ == "__main__":
     store_data = os.getenv("data")        
+    jdata = json.loads(store_data)
     print (f">>>>> changes: {store_data}")
-    store(store_data["changes"])
+    store(store_data["pr-changes"])
