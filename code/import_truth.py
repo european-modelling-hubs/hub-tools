ede8ec582a306f7f0ab075200f35baf7be07967d
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-12-04 17:44:50 +0100

    Update import_truth.py

--- code/import_truth.py
+++ code/import_truth.py
@@ -4,38 +4,38 @@ from urllib.request import urlopen
 from datetime import datetime, timedelta
 from isoweek import Week
 import pycountry
-​
+
 # Config
-​
+
 url = 'https://raw.githubusercontent.com/EU-ECDC/Respiratory_viruses_weekly_data/main/data/snapshots/{snapshot_date}_ILIARIRates.csv'
 out_files_ILI = ['target-data/ERVISS/latest-ILI_incidence.csv', 'target-data/ERVISS/ILI/{report_date}-ILI_incidence.csv']
 out_files_ARI = ['target-data/ERVISS/latest-ARI_incidence.csv', 'target-data/ERVISS/ARI/{report_date}-ILI_incidence.csv']
-​
-​
+
+
 # Build URL
-​
+
 today = datetime.now()
 days_to_last_friday = (today.weekday() - 4) % 7
 last_friday_date = today - timedelta(days=days_to_last_friday)
 snapshot_date=last_friday_date.date().isoformat()
-​
+
 url = url.format(snapshot_date=snapshot_date)
 try:
     response = urlopen(url)
 except urllib.error.HTTPError:
     sys.exit(1)
-​
-​
+
+
 # Read data
-​
+
 lines = [line.decode('utf-8') for line in response.readlines()]
-​
+
 csv_reader = csv.DictReader(lines, delimiter=',')
-​
+
 ILI_records = [('location', 'truth_date', 'year_week', 'value')]
 ARI_records = [('location', 'truth_date', 'year_week', 'value')]
-​
-​
+
+
 for row in csv_reader:
     if row['survtype'] != 'primary care syndromic' or row['age'] != 'total':
         continue
@@ -48,8 +48,8 @@ for row in csv_reader:
             ILI_records.append((country2, truth_date, row['yearweek'], value))
         case 'ARIconsultationrate':
             ARI_records.append((country2, truth_date, row['yearweek'], value))
-​
-​
+
+
 # write outpur files
 out_files_ILI = [of.format(report_date=snapshot_date) for of in out_files_ILI]
 out_files_ARI = [of.format(report_date=snapshot_date) for of in out_files_ARI]
@@ -59,7 +59,7 @@ for output_path in out_files_ILI:
     with open('./repo/' + output_path, 'w') as output_file:
         csv_writer = csv.writer(output_file)
         csv_writer.writerows(ILI_records)    
-​
+
 for output_path in out_files_ARI:
     with open('./repo/' + output_path, 'w') as output_file:
         csv_writer = csv.writer(output_file)
@@ -68,5 +68,3 @@ for output_path in out_files_ARI:
 env_file = os.getenv('GITHUB_OUTPUT')
 with open(env_file, "a") as outenv:
    outenv.write (f"imported_files={out_files_ILI + out_files_ARI}")
-​
-​
