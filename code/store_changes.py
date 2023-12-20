95200af4670298f10e68ff4871fc81e58245c022
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2023-12-21 00:17:16 +0100

    Update store_changes.py

--- code/store_changes.py
+++ code/store_changes.py
@@ -162,10 +162,10 @@ def store(to_store):
     for fchanged in fchanges:
                 
         # needed for different deepness of paths
-         if fchanged.startswith("model-output" + os.path.sep + "respicast-hubEnsemble"  + os.path.sep) or fchanged.startswith("model-output" + os.path.sep + "respicast-quantileBaseline"  + os.path.sep):
+        if fchanged.startswith("model-output" + os.path.sep + "respicast-hubEnsemble"  + os.path.sep) or fchanged.startswith("model-output" + os.path.sep + "respicast-quantileBaseline"  + os.path.sep):
             # add to ensemble
             ensemble_changes.append(fchanged)
-        if fchanged.startswith("model-output"):
+        elif fchanged.startswith("model-output"):
             # save model output
             model_changes.append(fchanged)
         elif fchanged.startswith("model-metadata"):
