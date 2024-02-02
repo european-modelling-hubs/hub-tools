fe86921bc3c022a7f58d1a4723c903e952eea63a
Paolo Milano <87755502+M-7th@users.noreply.github.com>
2024-02-02 13:05:55 +0100

    Update format_evaluation_file.py

--- code/format_evaluation_file.py
+++ code/format_evaluation_file.py
@@ -62,12 +62,12 @@ df_scores["rank"] = df_scores["rank"].astype(int)
 
 # save 
 max_origin_date = df_scores.origin_date.max()
-df_scores.to_csv(os.path.join(args.hub_path, f"model-evaluation/latest_forecast_scores.csv"), index=False)
+df_scores.to_csv(os.path.join(args.hub_path, f"model-evaluation/latest-forecast_scores.csv"), index=False)
 df_scores.to_csv(os.path.join(args.hub_path, f"model-evaluation/snapshots/{max_origin_date}-forecast_scores.csv"), index=False)
 
 
 env_file = os.getenv('GITHUB_OUTPUT')
 with open(env_file, "a") as outenv:
-   outenv.write (f"scoring_file_latest=model-evaluation/latest_forecast_scores.csv\n")
+   outenv.write (f"scoring_file_latest=model-evaluation/latest-forecast_scores.csv\n")
    outenv.write (f"scoring_file_snapshot=model-evaluation/snapshots/{max_origin_date}-forecast_scores.csv")
         
